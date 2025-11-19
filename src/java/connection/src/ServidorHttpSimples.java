76fc4e3f4ce71c3ae59ee0fd83c6056e55a0cff2
Gustavo Nunes <Gununes.tech7@gmail.com>
2025-11-19 16:18:57 -0300

    Produçã da Agenda e Arrumar Java

--- src/java/connection/src/ServidorHttpSimples.java
+++ src/java/connection/src/ServidorHttpSimples.java
@@ -9,38 +9,36 @@ public class ServidorHttpSimples {
         // Cria servidor HTTP escutando na porta 8080
         HttpServer servidor = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        /* INICIO DO CÓDIGO */
-        servidor.createContext("/", troca -> {
-            enviarArquivo(troca, "index.html", "text/html");
-            });
 
+        servidor.createContext("/styles.css", troca -> {
+            enviarArquivo(troca, "src/styles/styles.css", "text/css");
+        });
+
+        servidor.createContext("/global.css", troca -> {
+            enviarArquivo(troca, "src/styles/global.css", "text/css");
+        });
+
+        /* INICIO DO CÓDIGO */
+        //C:\Users\Aluno\Desktop\Dev\Laguna_School_Tech_Go\Login_e_senha.html
+        servidor.createContext("/", troca -> {
+            enviarArquivo(troca, "login_e_senha.html", "text/html");
+        });
         servidor.createContext("/professor", troca -> {
             enviarArquivo(troca, "professor.html", "text/html");
-
-            });
-
+        });
         servidor.createContext("/aluno", troca -> {
             enviarArquivo(troca, "aluno.html", "text/html");
-
         });
-
         servidor.createContext("/acessonegado", troca -> {
             enviarArquivo(troca, "acessonegado.html", "text/html");
-
         });
-
-        servidor.createContext("/estilo.css", troca -> {
-            enviarArquivo(troca, "estilo.css", "text/css");
-
-        });
-
 
 
 
 
         servidor.createContext("/login", troca -> {
             String query = troca.getRequestURI().getQuery();
-
+            //C:\Users\Aluno\Desktop\Dev\Laguna_School_Tech_Go\Login_e_senha.html
             String[] partes;
             partes = query.split("&");
 
@@ -50,13 +48,46 @@ public class ServidorHttpSimples {
             System.out.println(perfil);
 
 
+            //Professor/Instrutor
             if (usuario.equals("professor") && senha.equals("1234") &&  perfil.equals("professor")) {
-                System.out.println("Acesso autorizado");
+                System.out.println("Acesso autorizado: Professor/Instrutor");
                 troca.getResponseHeaders().set("Location" ,"/professor");
                 troca.sendResponseHeaders(302, -1); //envio para a rota
             }
+
+            if (usuario.equals("arieldias") && senha.equals("1234") &&  perfil.equals("professor")) {
+                System.out.println("Acesso autorizado: Professor Ariel Dias");
+                troca.getResponseHeaders().set("Location" ,"/professor");
+                troca.sendResponseHeaders(302, -1); //envio para a rota
+            }
+
+            if (usuario.equals("eduardofalabella") && senha.equals("1234") &&  perfil.equals("professor")) {
+                System.out.println("Acesso autorizado: Professor Eduardo Falabella");
+                troca.getResponseHeaders().set("Location" ,"/professor");
+                troca.sendResponseHeaders(302, -1); //envio para a rota
+            }
+
+            //Aluno
             if (usuario.equals("aluno") && senha.equals("1234") &&  perfil.equals("aluno")) {
-                System.out.println("Acesso autorizado");
+                System.out.println("Acesso autorizado: Aluno");
+                troca.getResponseHeaders().set("Location" ,"/aluno");
+                troca.sendResponseHeaders(302, -1); //envio para a rota
+            }
+
+            if (usuario.equals("gustavonunes") && senha.equals("1234") &&  perfil.equals("aluno")) {
+                System.out.println("Acesso autorizado: Aluno Gustavo Nunes");
+                troca.getResponseHeaders().set("Location" ,"/aluno");
+                troca.sendResponseHeaders(302, -1); //envio para a rota
+            }
+
+            if (usuario.equals("carinasouza") && senha.equals("1234") &&  perfil.equals("aluno")) {
+                System.out.println("Acesso autorizado: Aluna Carina Souza");
+                troca.getResponseHeaders().set("Location" ,"/aluno");
+                troca.sendResponseHeaders(302, -1); //envio para a rota
+            }
+
+            if (usuario.equals("luizatimporini") && senha.equals("1234") &&  perfil.equals("aluno")) {
+                System.out.println("Acesso autorizado: Aluna Luiza Timporini");
                 troca.getResponseHeaders().set("Location" ,"/aluno");
                 troca.sendResponseHeaders(302, -1); //envio para a rota
             }
@@ -65,9 +96,8 @@ public class ServidorHttpSimples {
                 System.out.println("Acesso negado");
                 troca.getResponseHeaders().set("Location" ,"/acessonegado");
                 troca.sendResponseHeaders(302, -1);
-                }
+            }
         });
-
 
         /* FIM DO CÓDIGO */
 
@@ -77,7 +107,8 @@ public class ServidorHttpSimples {
 
     // Envia um arquivo (HTML ou CSS)
     private static void enviarArquivo(com.sun.net.httpserver.HttpExchange troca, String caminho, String tipo) throws IOException {
-        File arquivo = new File("src/" + caminho);
+        File arquivo = new File(caminho);
+        System.out.println(" " + arquivo.getAbsolutePath());
         if (!arquivo.exists()) {
             System.out.println("Arquivo não encontrado: " + arquivo.getAbsolutePath());
         }
